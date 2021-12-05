a8569baca4b224e879c61dbd4d7c9c4b37ce32c9
João Fonseca <joaofvfonseca@gmail.com>
2021-12-05 06:22:25 +0000

    Initial premake config

---
+++ Unica/premake5.lua
@@ -0,0 +1,22 @@
+project "Unica"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++17"
+
+    targetdir ("../Build/" .. outputdir .. "/%{prj.name}")
+    objdir ("../Intermediate/" .. outputdir .. "%{prj.name}")
+
+    files
+    {
+        "Config/**.ini",
+        "Source/**.h",
+        "Source/**.cpp"
+    }
+
+filter "configurations:Debug"
+    defines "UNICA_DEBUG"
+    symbols "on"
+
+filter "configurations:Shipping"
+    defines "UNICA_SHIPPING"
+    optimize "on"
---
+++ premake5.lua
@@ -0,0 +1,13 @@
+workspace "Unica"
+    architecture "x64"
+    startproject "Unica"
+
+    configurations
+    {
+        "Debug",
+        "Shipping"
+    }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+include "Unica"
