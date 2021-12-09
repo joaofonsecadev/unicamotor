798c12fbbecf5992699f325c1d0c715de8991ba7
João Fonseca <joaofvfonseca@gmail.com>
2021-12-09 22:28:47 +0000

    Using custom typedefs

---
+++ Unica/Source/Types.h
@@ -0,0 +1,17 @@
+// Copyright João Fonseca, All Rights Reserved.
+
+#pragma once
+
+#include <string>
+
+typedef signed char         int8;
+typedef short               int16;
+typedef int                 int32;
+typedef long long           int64;
+
+typedef unsigned char       uint8;
+typedef unsigned short      uint16;
+typedef unsigned int        uint32;
+typedef unsigned long long  uint64;
+
+typedef std::string         string;
