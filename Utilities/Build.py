edb545b5b9a3d1f79fc043cd17512941f3896220
João Fonseca <joaofvfonseca@gmail.com>
2022-07-24 13:24:38 +0100

    Replacing Premake with CMake

--- Utilities/Build.py
+++ Utilities/Build.py
@@ -1,21 +1,20 @@
 import os
 import sys
 import time
+import shutil
 import platform
 import subprocess
 
 startTime = time.time()
 
 helpText = "\n\
-Run the script without any commands to generate project files adequate for your system (Windows: Visual Studio 2019; MacOS: XCode 4).\n\n\
+Run the script without any commands to generate project files adequate for your system.\n\n\
 The following arguments are accepted:\n\
--h: Print this text.\n\
--gmake: Generate GNU Make project files.\n"
+-h: Print this text.\n"
 
 # 'Linux', 'Darwin', 'Java', 'Windows'
 systemType = platform.system();
 
-premakeTool = ""
 projectType = ""
 argList = sys.argv[1:]
 buildHelperDir = os.path.dirname(__file__)
@@ -24,8 +23,19 @@ buildHelperDir = os.path.dirname(__file__)
 os.chdir(buildHelperDir)
 os.chdir('..')
 
+def ensureCMakeExists():
+    cmakePath = shutil.which("cmake")
+    if cmakePath is None:
+        print("No CMake found in PATH, aborting")
+        endExecution()
+    return cmakePath
+
 def generateProject():
-    subprocess.run([premakeTool, projectType])
+    subprocess.run([ensureCMakeExists(),
+        "-S.",
+        "-B{0}/Intermediate/CMake".format(os.getcwd()),
+        projectType
+    ])
 
 def endExecution():
     print("Execution finished in {0} seconds".format(time.time() - startTime))
@@ -36,14 +46,12 @@ if sys.version_info < (3, 10):
     endExecution()
 
 if systemType == 'Windows':
-    premakeTool = os.path.join(buildHelperDir, 'premake5\premake5.exe')
-    projectType = 'vs2019'
+    projectType = '-GVisual Studio 16 2019'
 elif systemType == 'Darwin':
-    premakeTool = os.path.join(buildHelperDir, 'premake5/premake5')
-    projectType = 'xcode4'
+    projectType = '-GXcode'
 else:
-    print('System "{0}" not supported'.format(systemType))
-    endExecution()
+    print('System "{0}" not supported by default. Defaulting to Make Files'.format(systemType))
+    projectType = '-GUnix Makefiles'
 
 if (len(argList) < 1):
     generateProject()
@@ -54,17 +62,21 @@ for arg in argList:
         case "-h":
             print(helpText)
 
-        case "-vstudio":
-            projectType = 'vs2019'
+        case "-vs2019":
+            projectType = '-GVisual Studio 16 2019'
+
+        case "-vs2022":
+            projectType = '-GVisual Studio 17 2022'
 
         case "-xcode":
-            projectType = 'xcode4'
+            projectType = '-GXcode'
 
-        case "-cmake":
-            projectType = 'cmake'
+        case "-make":
+            projectType = '-GUnix Makefiles'
 
-        case "-gmake":
-            projectType = 'gmake2'
+        case "-clean":
+            subprocess.run(["git", "clean", "-fxd"])
+            endExecution()
 
         case _:
             print("Invalid command.\nTry -h for some useful information on how to run this script.")
