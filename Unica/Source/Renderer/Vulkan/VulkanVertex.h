5e27cae0019da2fe4870093f05cca795bc6546c4
João Fonseca <joaofvfonseca@gmail.com>
2023-05-11 13:58:06 +0100

    Basic vulkan vertex

---
+++ Unica/Source/Renderer/Vulkan/VulkanVertex.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include <array>
+
+#include "vulkan/vulkan_core.h"
+#include "glm/vec2.hpp"
+#include "glm/vec3.hpp"
+
+struct VulkanVertex
+{
+    glm::vec2 Position;
+    glm::vec3 Color;
+
+    static VkVertexInputBindingDescription GetBindingDescription();
+    static std::array<VkVertexInputAttributeDescription, 2> GetAttributeDescriptions();
+};
