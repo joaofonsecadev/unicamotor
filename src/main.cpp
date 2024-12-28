514bd952a67ebde402ed48dab84e717263a0fe44
João Fonseca <joaofvfonseca@gmail.com>
2024-12-28 03:02:22 +0000

    Remove unwanted test code

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include "core/profiling.h"
 
-#include "core/string.h"
 #include "motor/unicamotor.h"
 
 int main(int argc, char* argv[])
@@ -9,8 +8,6 @@ int main(int argc, char* argv[])
     while (!engine.HasExitBeenRequested())
     {
         UnicaProf_FrameMarkStart("EngineLoop");
-        unc::string teste_string = unc::string("o meu nome e rebeca e o teu qual e");
-        unc::string tesss = unc::string("quem me dera algo doce");
         UnicaProf_FrameMarkEnd("EngineLoop");
     }
 
